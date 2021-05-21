d297f6dd965fd8442e9b8171ee6e1fc9630b4e99
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-21 17:11:09 -0400

    Cors and express.json added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 // Crear el servidor / aplicación de express
 const app = express();
 
 // Middlewares
+app.use(cors());
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
 app.use('/api/auth', require('./routes/auth'));
 
 app.listen(4000, () => {
