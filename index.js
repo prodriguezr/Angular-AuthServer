6d76469af3b55f21a7545997f5d07327c0d03a77
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-21 18:11:41 -0400

    Public folder and page added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 // Crear el servidor / aplicación de express
 const app = express();
@@ -10,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
+app.use(express.static('public'));
 app.use('/api/auth', require('./routes/auth'));
 
-app.listen(4000, () => {
-    console.log(`Server running on port ${4000}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
