bbd689b1f0034de826e73b5d653622ed5dc54869
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-21 16:30:25 -0400

    Basic get response added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello form Node!');
+const express = require('express');
+
+// Crear el servidor / aplicación de express
+const app = express();
+
+// GET
+app.get('/', (req, res) => {
+    res.json({ ok: true, msg: 'Is OK', uid: 12345 });
+});
+
+
+app.listen(4000, () => {
+    console.log(`Server running on port ${4000}`);
+});
