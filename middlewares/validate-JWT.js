1878faf614ddf09ac70599c8c072acef3f31e54f
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-22 02:00:11 -0400

    Create middleware for validate JWT and renew method modified

--- middlewares/validate-JWT.js
+++ middlewares/validate-JWT.js
@@ -17,6 +17,7 @@ const validateJWT = (req, res = response, next) => {
 
         req.uid = uid;
         req.name = name;
+        
     } catch (err) {
         return res.status(401).json({
             ok: false,
