88d15dced42e0b101f98e8cb17e705c074250234
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-21 22:05:18 -0400

    Register controller modified / User saved (basic)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,44 @@
 const { response } = require('express');
+const User = require('../models/User');
 
-const registerUser = ( req, res = response ) => {
+const registerUser = async( req, res = response ) => {
     const { name, email, password } = req.body;
 
-    return res.json({
-        ok: true,
-        msg: 'Create user /register'
-    });
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User with these email already exists'
+            });
+        }
+
+        // Create user from model
+        const dbUser = new User(req.body);
+
+        // Verify email
+
+        // Hash password
+
+        // Generate the JWT
+
+        // Save 
+        await dbUser.save();
+
+        // Generate successfull response
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name
+        });
+    } catch (error) {        
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Please contact the Administrator',
+        });
+    }
 }
 
 const userLogin = (req, res = response) => {
