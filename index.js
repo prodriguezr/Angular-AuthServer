0f9d713cbd1f51c852ce508fcc74612aa0840a5b
Pablo Rodríguez <prodrigu75@gmail.com>
2021-05-23 17:31:14 -0400

    AuthServer finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./db/config');
+const { response } = require('express');
+const path = require('path');
 require('dotenv').config();
 
 // Crear el servidor / aplicación de express
@@ -16,6 +18,10 @@ app.use(express.json());
 // Rutas
 app.use(express.static('public'));
 app.use('/api/auth', require('./routes/auth'));
+
+app.get('*', (req, res = response) => {
+    res.sendfile(path.resolve(__dirname, 'public/index.html'))
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
